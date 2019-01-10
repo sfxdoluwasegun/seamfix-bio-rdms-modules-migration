ee61e7d02e249cf6b4dbae78a529a3408a17e68d
kingsley ezeokeke <kezeokeke@seamfix.com>
2019-01-10 11:07:19 +0100

    migrate signupMedium field in biocloud users ext, migrate createdDate field in prospective users

--- bio-migration-job/src/main/java/com/seamfix/bio/job/jpa/dao/ProspectiveUsersRepository.java
+++ bio-migration-job/src/main/java/com/seamfix/bio/job/jpa/dao/ProspectiveUsersRepository.java
@@ -2,6 +2,16 @@ package com.seamfix.bio.job.jpa.dao;
 
 import com.seamfix.bio.entities.ProspectiveUsers;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProspectiveUsersRepository extends JpaRepository<ProspectiveUsers, Long> {
+    ProspectiveUsers findByEmail(String email);
+
+    @Modifying
+    @Query("UPDATE ProspectiveUsers p SET p.active = :active WHERE p.email = :email")
+    int updateActiveStatus(@Param("active") boolean active, @Param("email") String email);
 }
